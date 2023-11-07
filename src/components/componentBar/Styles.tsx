df591c679883b3a166cf395831fa6e6e11db9cbf
Felipe Cabral <cabral009@outlook.com>
2023-11-06 21:29:43 -0300

    Home, navbar, upbar, login and image update
    Created new component: upbar, who will be on the top of the app, updating the colors and border of the navbar and switch the logo app on the begin of app

--- src/components/componentBar/Styles.tsx
+++ src/components/componentBar/Styles.tsx
@@ -2,14 +2,13 @@ import { StyleSheet } from "react-native";
 
 const Styles = StyleSheet.create({
   fixedContainer: {
-    position: "relative",
-    marginBottom: "4%",
+    position: "absolute",
+    marginTop: "196%",
     width: "100%",
     marginLeft: "0%",
-    height: "8%",
-    borderTopColor: "#d5d5d5",
-    borderTopWidth: 0.2,
+    height: "6%",
     flexDirection: "row",
+    zIndex: 1,
   },
   iconContainer: {
     marginTop: "4%",
@@ -27,7 +26,7 @@ const Styles = StyleSheet.create({
     borderColor: "black",
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#cc0000",
+    backgroundColor: "#fbc500",
   },
 });
 
---
+++ src/components/componentUpBar/ComponentUpBar.tsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { View, TouchableOpacity, Text } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import Styles from "./Styles";
+
+// Criando a barra de componentes em baixo
+const ComponentUpBar = () => {
+  return (
+    // Criando o container principal do component
+    <View style={Styles.fixedContainer}>
+      <Text style={Styles.appName}>EasyMovie</Text>
+      {/* Container do icon */}
+      <View style={Styles.iconContainer}>
+        <TouchableOpacity>
+          <Icon name="search" size={20} color="#FBC500" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+// Exportando o componente
+export default ComponentUpBar;
