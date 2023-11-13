acc57a11c04e8f50672055ee34f5d19b5991552c
Felipe Cabral <cabral009@outlook.com>
2023-11-13 15:34:06 -0300

    Enabled false

--- App.tsx
+++ App.tsx
@@ -5,32 +5,65 @@ import Login from "./src/screens/login/Login";
 import JoinNow from "./src/screens/joinNow/joinNow";
 import CreateAccount from "./src/screens/register/CreateAccount";
 import Home from "./src/screens/home/Home";
-import Search from './src/screens/search/Search';
+import Search from "./src/screens/search/Search";
 import PopularMovies from "./src/screens/popularMovies/PopularMovies";
 import Profile from "./src/screens/profile/Profile";
-import   Edit   from "./src/screens/Edit"
+import Edit from "./src/screens/Edit";
 import { LogBox } from "react-native";
 
-LogBox.ignoreLogs(['ViewPropTypes will be removed']);
+LogBox.ignoreLogs(["ViewPropTypes will be removed"]);
 
 // Código main do app
 const App = (): JSX.Element => {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="JoinNow">
+      <Stack.Navigator
+        initialRouteName="JoinNow"
+        screenOptions={{ 
+          gestureEnabled: false, 
+        }}
+      >
         <Stack.Screen
           name="JoinNow"
           component={JoinNow}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-        <Stack.Screen name="CreateAccount" component={CreateAccount} options={{ headerShown: false }} />
-        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-        <Stack.Screen name="Search" component={Search} options={{ headerShown: false }} />
-        <Stack.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
-        <Stack.Screen name="Edit" component={Edit} options={{ headerShown: false }} />
-        <Stack.Screen name="PopularMovies" component={PopularMovies} options={{ headerShown: false }} />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="CreateAccount"
+          component={CreateAccount}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Search"
+          component={Search}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Edit"
+          component={Edit}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="PopularMovies"
+          component={PopularMovies}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
